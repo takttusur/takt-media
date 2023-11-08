91b2177abaaa666fc1137dc606ae482be65cbc7d
Nikita Martsenkovsky <martsenkovskyn@gmail.com>
2023-11-08 10:20:09 +0700

    Created TaktTusur.Media.BackgroundCrawler project.

---
+++ TaktTusur.Media.BackgroundCrawler/Program.cs
@@ -0,0 +1,10 @@
+using TaktTusur.Media.BackgroundCrawler;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
