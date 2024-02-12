b1d19e4ffd1418de68ab328cad1c4b0a0c1b12e0
Nikita Martsenkovsky <martsenkovskyn@gmail.com>
2024-02-12 11:26:08 +0700

    Generated slasses ignoring added.

---
+++ TaktTusur.Media.Clients.VkApi.Generator/Program.cs
@@ -0,0 +1,16 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+var directoryPath = @"..\..\TaktTusur.Media.Clients.VkApi\VkSchema\";
+var outputPath = @"..\..\TaktTusur.Media.Clients.VkApi\VkSchema\Generated\";
+
+foreach (var filePath in Directory.GetFiles(directoryPath, "*.json"))
+{
+    var document = JsonSchema.FromFileAsync(filePath).Result;
+    var generator = new CSharpGenerator(document);
+    var file = generator.GenerateFile();
+
+    var outputFilePath = Path.Combine(outputPath, Path.GetFileNameWithoutExtension(filePath) + ".cs");
+
+    File.WriteAllText(outputFilePath, file);
+}
