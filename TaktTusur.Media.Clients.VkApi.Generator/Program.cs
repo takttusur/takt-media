d2f7a5c6a3515ba93aa66ddafb2e78f4a7a4af0f
Nikita Martsenkovsky <martsenkovskyn@gmail.com>
2024-02-20 09:57:49 +0700

    Individual namespaces and automatic directory creation added.

--- TaktTusur.Media.Clients.VkApi.Generator/Program.cs
+++ TaktTusur.Media.Clients.VkApi.Generator/Program.cs
@@ -4,15 +4,23 @@ using NJsonSchema.CodeGeneration.CSharp;
 var directoryPath = @"..\..\..\..\TaktTusur.Media.Clients.VkApi\VkSchema\";
 var outputPath = @"..\..\..\..\TaktTusur.Media.Clients.VkApi\VkSchema\Generated\";
 
+if (!Directory.Exists(outputPath))
+    Directory.CreateDirectory(outputPath);
+
 foreach (var filePath in Directory.GetFiles(directoryPath, "*.json", SearchOption.AllDirectories))
 {
+    var classNamespace = filePath.Replace(directoryPath, "").Replace(".json", "");
+
+    if (classNamespace.IndexOf(@"\") > 0)
+        Directory.CreateDirectory(Path.Combine(outputPath, classNamespace[..classNamespace.IndexOf(@"\")]));
+
     var json = File.ReadAllText(filePath);
     var jsonSchema = JsonSchema.FromSampleJson(json);
-    var settings = new CSharpGeneratorSettings() { Namespace = "JsonSchemeGeneratedClasses" };
+
+    var settings = new CSharpGeneratorSettings() { Namespace = $"Generated.{classNamespace.Replace(@"\", ".")}" };
     var generator = new CSharpGenerator(jsonSchema, settings);
     var file = generator.GenerateFile();
 
-    var outputFilePath = Path.Combine(outputPath, filePath.Replace(directoryPath, "").Replace(".json", ".cs"));
-
+    var outputFilePath = Path.Combine(outputPath, $"{classNamespace}.cs");
     File.WriteAllText(outputFilePath, file);
 }
